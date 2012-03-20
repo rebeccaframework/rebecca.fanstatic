ec034828672d840eea477c6618d161e896ae0902
Atsushi Odagiri <aodagx@gmail.com>
2012-03-20 00:11:22 -0700

    garden

--- setup.py
+++ setup.py
@@ -1,8 +1,18 @@
 from setuptools import setup, find_packages
+import os
+
+__version__ = '0.1'
+__author__ = 'Atsushi Odagiri'
 
 requires = [
-    "pyramid",
     "fanstatic",
+]
+
+testing_requires = [
+    "nose",
+    "coverage",
+    "mock",
+    "webtest",
 ]
 
 try:
@@ -18,9 +28,31 @@ points = {
         "rebecca.fanstatic.dummy_library=rebecca.fanstatic.tests.dummylibrary:dummy_library",
     ],
 }
+here = os.path.dirname(__file__)
+
+def _read(name):
+    try:
+        return open(os.path.join(here, name)).read()
+    except:
+        return ""
+
+        
+readme = _read("README.txt")
+changes = _read("CHANGES.txt")
 
 setup(name="rebecca.fanstatic",
+    version=__version__,
+    author=__author__,
+    author_email="aodagx@gmail.com",
+    url="http://github.com/rebeccaframework/rebecca.fanstatic",
+
     install_requires=requires,
     entry_points=points,
+    extras_require= {
+        "testing": testing_requires,
+    },
+    description="fanstatic utilities",
+    long_description=readme+"\r\n"+changes,
+    license="MIT",
 )
 
